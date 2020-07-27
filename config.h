0169e9cc1e630837dad35349ac87484196defaaf
Daksh Miglani <hello@dak.sh>
2020-07-27 21:51:14 +0530

    use iosevka

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=19:antialias=true:autohint=true";
+static char *font = "Iosevka Nerd Font:pixelsize=21:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-"JetBrainsMono Nerd Font:pixelsize=19:antialias=true:autohint=true"
+"JoyPixels:pixelsize=19:antialias=true:autohint=true"
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
